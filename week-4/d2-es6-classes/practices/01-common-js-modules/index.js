68a90827e9dc2ca60c97f4b18773a610d66a1d79
Mattchu18 <mattchu18@gmail.com>
2023-02-14 09:42:19 -0800

    d2 work

--- week-4/d2-es6-classes/practices/01-common-js-modules/index.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/index.js
@@ -1,6 +1,18 @@
-
+const sayHelloTo = require('./send-messages/say-hello-to')
 // Your code here
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato')
 
+const {         //importing from index.js in messagesaliasiing
+    msg1,
+    msg2,
+    msg3
+} = require('./messages')
+
+const {
+    message1: msg1,
+    message2: msg2,
+    message3: msg3
+} = require('./messages')
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- week-4/d2-es6-classes/practices/01-common-js-modules/messages/index.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/messages/index.js
@@ -1,3 +1,11 @@
 const message1 = "You are looking very spudly today!";
 const message2 = "There's a good deal on mattresses tomorrow.";
 const message3 = "Do you like to do things in unnecessarily complicated ways?";
+
+
+//we had to export it by aliasing
+module.exports = { msg1: message1, msg2: message2, msg3: message3}
+
+
+
+module.exports = { message,1, message2, message3}
